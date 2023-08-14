6e330912a5eb4faaad86ce2c082a283a0c917387
Karthik260404 <140727545+Karthik260404@users.noreply.github.com>
2023-08-14 01:37:04 -0700

    Update print.js

--- print.js
+++ print.js
@@ -1,5 +1,5 @@
 const calcy=require('./calc')
-console.log('sum:'+calcy.sum(4,5))
+console.log('sum:'+calcy.sum(4,10))
 console.log('sub:'+calcy.sub(4,5))
 console.log('mul:'+calcy.mul(4,5))
 console.log('div:'+calcy.div(10,5))
